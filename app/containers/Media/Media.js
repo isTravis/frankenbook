229d7907b710f7f52416f1fec394cb0c3fc00c56
Travis Rich <travis.s.rich@gmail.com>
2018-03-15 10:39:39 -0400

    Updates to Media Page

--- app/containers/Media/Media.js
+++ app/containers/Media/Media.js
@@ -16,6 +16,12 @@ class Media extends Component {
 						<div className={'col-12'}>
 							<h1>Media</h1>
 
+							<h2>Step into the lab to build solutions to today’s vexing questions</h2>
+							<a href="http://www.bluecadet.com/projects/modern-monsters/#/">
+								<img alt="Modern Monsters" src="/images/modernMonsters.jpg" width="100%" />
+							</a>
+							<p><a href="http://www.bluecadet.com/projects/modern-monsters/#/">Click to step into the lab.</a></p>
+
 							<h2>A Year Without a Summer</h2>
 							<iframe width="100%" height="300" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/269203861&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true" />
 
@@ -23,7 +29,7 @@ class Media extends Component {
 							<a href="https://lizfiacco.itch.io/lab-assistant">
 								<img alt="Lab Assistant" src="/images/lab-assistant.jpg" width="100%" />
 							</a>
-							<p><a href="https://lizfiacco.itch.io/lab-assistant">Click to play Lab Assistant</a></p>
+							<p><a href="https://lizfiacco.itch.io/lab-assistant">Click to play Lab Assistant.</a></p>
 						</div>
 					</div>
 				</div>
